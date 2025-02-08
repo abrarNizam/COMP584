ff1073d567edab83c0caa6c503b625e8d1c47bae
Nizam <s-abrar.nizam.017@my.csun.edu>
2025-02-07 18:13:09 -0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { WeatherForecast } from './weather-forecast';
+import { environment } from '../environments/environment.development';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: true,
+  styleUrl: './app.component.scss',
+  imports: [
+    NavBarComponent, 
+    RouterOutlet
+  ]
+})
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+  
+
+  title = 'dominicangular.client';
+}
