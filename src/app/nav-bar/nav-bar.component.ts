89eff8af81204a774e8759515ed262eca3a8ee79
Nizam <s-abrar.nizam.017@my.csun.edu>
2025-03-07 18:16:48 -0800

    updates

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [
+    RouterLink, 
+    MatToolbarModule, 
+    MatIconModule
+    
+  ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
