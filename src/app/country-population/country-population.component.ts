5f37f12fa92c3ca0b1d2d0e78e55af5b76fbb0cd
Nizam <s-abrar.nizam.017@my.csun.edu>
2025-03-14 18:12:18 -0700

    updates

---
+++ src/app/country-population/country-population.component.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CountryPopulation } from '../country-population';
+import { environment } from '../../environments/environment.development';
+
+@Component({
+  selector: 'app-country-population',
+  imports: [RouterLink],
+  templateUrl: './country-population.component.html',
+  styleUrl: './country-population.component.scss'
+})
+export class CountryPopulationComponent implements OnInit{
+
+  public population: CountryPopulation | undefined;
+    constructor(private http: HttpClient, private activatedroute: ActivatedRoute ){}
+  
+    ngOnInit(): void {
+      this.getCountryPopulation
+    }
+  
+  
+     getCountryPopulation() {
+      let id = this.activatedroute.snapshot.paramMap.get("id");
+        this.http.get<CountryPopulation>(`${environment.baseUrl}/api/Countries/GetPopulation/${id}`).subscribe({
+          next: result => this.population = result, 
+          error: error => console.error(error)
+        }  
+        );
+      }
+}
